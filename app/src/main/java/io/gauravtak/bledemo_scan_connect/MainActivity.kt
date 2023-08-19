559354615f3b0bcccddcdd712237a2c39654d7a3
Gaurav Tak <gaurav.tak@roro.io>
2023-08-20 00:58:32 +0530

    initial commit

---
+++ app/src/main/java/io/gauravtak/bledemo_scan_connect/MainActivity.kt
@@ -0,0 +1,11 @@
+package io.gauravtak.bledemo_scan_connect
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
